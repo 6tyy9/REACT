8999fe1329fd58486f99bd20a676bbf82f810b98
Shivanshu <rawat.shivanshu007@gmail.com>
2024-09-23 15:23:30 +0530

    06CurrencyConverter

---
+++ 06CurrencyConverter/src/hooks/useCurrencyInfo.js
@@ -0,0 +1,15 @@
+import { useEffect, useState } from "react";
+
+function useCurrencyInfo(currency){
+    const [data, setData] =useState({}) // passing empty object so that if no value is passed still the project will not crash as we can traverse the obj with the help of a loop
+     useEffect(()=>{
+        fetch(`https://cdn.jsdelivr.net/npm/@fawazahmed0/
+            currency-api@2024-03-06/v1/currencies/${currency}.json`)
+            .then((res)=>res.json())
+            .then((res)=>setData(res[currency]))
+            console.log(data)
+     },[currency]) // currrency is dependencny bcz whenever there is change in value of the currency we need to call th fn again
+      console.log(data);
+      return data
+    }
+    export default useCurrencyInfo;
