8dc25c3c5ad4fd426891fa084bfd3e9fc8a3c067
Shivanshu <rawat.shivanshu007@gmail.com>
2024-11-29 13:59:36 +0530

    work on context finished

--- 10todocontextLocal/src/context/TodoContext.js
+++ 10todocontextLocal/src/context/TodoContext.js
@@ -1,9 +1,22 @@
 import { useContext, createContext } from "react";
 
-export const TodoContext =createContext({})
+export const TodoContext =createContext({
+    todos :[
+        {
+            id:1,
+            todo:"Todo msg",
+            completed:false,
+        }
+    ],
+    addTodo: (todo)=>{},
+    updatedTodo: (id,todo)=>{},
+    deleteTodo:(id)=>{},
+    toggleComplete:(id)=>{}
+})
 
+//exporting a hook
 export const useTodo =() =>{
-    return useContext(TodoContext)
+    return useContext(TodoContext) //whenver we use useContext we need to give it some context
 }
 
-export const Todoprovider =TodoContext.Provider 
+export const TodoProvider =TodoContext.Provider 
